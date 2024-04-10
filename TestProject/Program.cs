f28dfc793ce08db5ab265c8a5f323c95ba68622f
Mmishaaa <kkochin2528@gmail.com>
2024-04-10 17:22:05 +0300

    feat: implement Serilog

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,6 +1,11 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +20,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
